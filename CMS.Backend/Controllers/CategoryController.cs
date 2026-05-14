a7f79704d1008297e991c83a5f0f287102bcb5ca
quitran1211 <kimquiforwork1211@gmail.com>
2026-05-14 15:53:33 +0700

    hoan thanh buoi 2

--- CMS.Backend/Controllers/CategoryController.cs
+++ CMS.Backend/Controllers/CategoryController.cs
@@ -1,19 +1,26 @@
-﻿using CMS.Data.Entities;
+﻿using CMS.Data;
+using CMS.Data.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CMS.Backend.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        // "Tiêm" kết nối vào Controller
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            // Tạo danh sách dữ liệu mẫu trực tiếp trong code
-            var list = new List<Category> {
-            new Category { Id = 1, Name = "Tin Công Nghệ", Description = "Review Laptop, AI" },
-            new Category { Id = 2, Name = "Giáo Dục", Description = "Thông tin tuyển sinh" }
-        };
-            return View(list); // Gửi danh sách này sang giao diện
+            // Lấy dữ liệu THẬT từ bảng Categories trong SQL
+            var data = _context.Categories.ToList();
+            return View(data);
         }
+
 
     }
 }
---
+++ CMS.Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using CMS.Data.Entities;
+
+namespace CMS.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        // Khai báo các bảng dữ liệu
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CategoryProduct> CategoriesProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
+
