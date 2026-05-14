3b3a34266306e53b49c36fd32f8f30209688b763
quitran1211 <kimquiforwork1211@gmail.com>
2026-05-14 14:35:43 +0700

    Add project files.

---
+++ CMS.Backend/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using CMS.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Backend.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Tạo danh sách dữ liệu mẫu trực tiếp trong code
+            var list = new List<Category> {
+            new Category { Id = 1, Name = "Tin Công Nghệ", Description = "Review Laptop, AI" },
+            new Category { Id = 2, Name = "Giáo Dục", Description = "Thông tin tuyển sinh" }
+        };
+            return View(list); // Gửi danh sách này sang giao diện
+        }
+
+    }
+}
---
+++ CMS.Data/Entities/Category.cs
@@ -0,0 +1,28 @@
+﻿/*
+ *Sinh vien: Tran Thi Kim Qui
+ *Ma sv: 2123110042
+ *Ngay tao: 14-05-2026
+ *Version: 1.0
+ *
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } // Tên danh mục (vd: Tin Giáo Dục)
+        public string Description { get; set; }
+
+        // Quan hệ: Một danh mục có nhiều bài viết
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+
+}
